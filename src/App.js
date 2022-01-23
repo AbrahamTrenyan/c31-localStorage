7da29666dc2ebafbaf055f3e52b5196c133a9721
abraham <abrutrenyan049@gmail.com>
2022-01-23 17:06:11 -0300

    deployyy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Agregar />} />
-        <Route path="/productos" element={<Productos />} />
+        <Route path="/c31-localStorage/productos" element={<Productos />} />
       </Routes>
     </Router>
   );
